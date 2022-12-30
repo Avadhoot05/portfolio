84b76113283014a5605f91f8cc44fa345d34c4a3
Avadhoot Khedekar <48721257+Avadhoot05@users.noreply.github.com>
2022-12-30 13:19:01 +0530

    changed form endpoint

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 let project_rows = document.querySelectorAll('.project-rows'); //element
-let overlay_box = document.querySelectorAll('.projects .overlay-box'); //element
+let overlay_box = document.querySelectorAll('.overlay-box'); //element
 const ham = document.getElementsByClassName("ham")[0];
 if(ham)
 	ham.addEventListener("click", onHamClick);
@@ -41,33 +41,51 @@ const opacityAnimation = (targetname, duration, easing, delay) => {
 	});
 };
 
-var waypoint = new Waypoint({
-	element: project_rows[0],
-	handler: function () {
-		overlay_box[0].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
-		overlay_box[1].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
-	},
-	offset: 700,
-});
+// ---new project UI
+
+
+
+const overlayWidgets = ["project1", "project2", "project3", "zeus-content", "brandyse-content", "roborise-content", "rrc-content"];
+
+
+for(let i = 0; i < overlayWidgets.length; i++)
+{
+	var waypoint = new Waypoint({
+		element: document.getElementById(overlayWidgets[i]),
+		handler: function () {
+			overlay_box[i].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
+		},
+		offset: 700,
+	});
+}
+
+// var waypoint = new Waypoint({
+// 	element: document.getElementById("project1"),
+// 	handler: function () {
+// 		overlay_box[0].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
+// 	},
+// 	offset: 700,
+// });
+
+// var waypoint = new Waypoint({
+// 	element: document.getElementById("project2"),
+// 	handler: function () {
+// 		overlay_box[1].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
+// 	},
+// 	offset: 700,
+// });
+
+// var waypoint = new Waypoint({
+// 	element: document.getElementById("project3"),
+// 	handler: function () {
+// 		overlay_box[2].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
+// 	},
+// 	offset: 700,
+// });
+
+
 
 //-----------------------------------------------------------------------------------------
-var waypoint = new Waypoint({
-	element: project_rows[1],
-	handler: function () {
-		overlay_box[2].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
-		overlay_box[3].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
-	},
-	offset: 700,
-});
-
-waypoint = new Waypoint({
-	element: project_rows[2],
-	handler: function () {
-		overlay_box[4].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
-		overlay_box[5].style.animation = 'right-to-left 0.5s ease 0.1s forwards';
-	},
-	offset: 700,
-});
 
 waypoint = new Waypoint({
 	element: document.querySelector('.skills'),
@@ -118,35 +136,33 @@ setTimeout(() => {
 	}, 30);
 }, 3000);
 
-
-
 /*---------------------------Form Submission-------------------------------*/
-var $form = $('form#google-sheet'),
-	url = 'https://script.google.com/macros/s/AKfycbzZLnHswcaH7vPS5S6wpa1I-wQxcIo0VKNHlIdYbR6pwxIVBYXzr_q77vUuncI2nn1f8Q/exec';
+// var $form = $('form#google-sheet'),
+// 	url = 'https://script.google.com/macros/s/AKfycbzZLnHswcaH7vPS5S6wpa1I-wQxcIo0VKNHlIdYbR6pwxIVBYXzr_q77vUuncI2nn1f8Q/exec';
 
-$('#sbmt').on('click', function (e) {
-	e.preventDefault();
+// $('#sbmt').on('click', function (e) {
+// 	e.preventDefault();
 
-	let form_fields = [document.querySelector('#fname'), document.querySelector('#lname'), document.querySelector('#email'), document.querySelector('#message')];
-	let ct = 0;
-	[$('#fname').val(), $('#lname').val(), $('#email').val(), $('#message').val()].forEach((element, index) => {
-		if (element.length === 0) {
-			ct += 1;
-			if (localStorage.getItem('darkflag') != 'enabled') form_fields[index].style.outline = '1px solid #cf0061';
-			else form_fields[index].style.outline = '1px solid #9d86e9';
-		} else form_fields[index].style.outline = 'none';
-	});
+// 	let form_fields = [document.querySelector('#fname'), document.querySelector('#lname'), document.querySelector('#email'), document.querySelector('#message')];
+// 	let ct = 0;
+// 	[$('#fname').val(), $('#lname').val(), $('#email').val(), $('#message').val()].forEach((element, index) => {
+// 		if (element.length === 0) {
+// 			ct += 1;
+// 			if (localStorage.getItem('darkflag') != 'enabled') form_fields[index].style.outline = '1px solid #cf0061';
+// 			else form_fields[index].style.outline = '1px solid #9d86e9';
+// 		} else form_fields[index].style.outline = 'none';
+// 	});
 
-	if (ct) return false;
+// 	if (ct) return false;
 
-	form_fields.forEach((element) => {
-		element.style.outline = 'none';
-	});
+// 	form_fields.forEach((element) => {
+// 		element.style.outline = 'none';
+// 	});
 
-	var jqxhr = $.ajax({
-		url: url,
-		method: 'GET',
-		dataType: 'json',
-		data: $form.serializeObject(),
-	}).success((document.querySelector('.ack').style.opacity = '1'));
-});
+// 	var jqxhr = $.ajax({
+// 		url: url,
+// 		method: 'GET',
+// 		dataType: 'json',
+// 		data: $form.serializeObject(),
+// 	}).success((document.querySelector('.ack').style.opacity = '1'));
+// });
